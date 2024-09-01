b2f85d8f3383bdbe9effd1cab0363d64dbe4002c
Mayank Singh <166054881+s2002mayank@users.noreply.github.com>
2024-09-02 01:03:12 +0530

    Update settings.py

--- src/CodeVerse/settings.py
+++ src/CodeVerse/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
